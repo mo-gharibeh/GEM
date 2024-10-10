1da33c42ecf56cbf421456872bef67a5b44ba69a
dimamohamad <deemaahmad189@gmail.com>
2024-10-10 18:09:57 +0300

    add meals

--- gem.client/src/app/app.module.ts
+++ gem.client/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { ContactComponent } from './newProject8/yousef/contact/contact.component
     HomeComponent,
     NavbarComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
     NavbarComponent,
     TipsComponent,
     MealsComponent,
@@ -34,6 +34,9 @@ import { ContactComponent } from './newProject8/yousef/contact/contact.component
       },
       {
         path: 'Tips', component: TipsComponent
+      },
+      {
+        path: 'Meals', component: MealsComponent
       }
 
 
