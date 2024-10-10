2f025cde64ea8dd342ce29396a1a094aba0caa36
Hadeel-Alzoubi <hadeelzoubi12@gmail.com>
2024-10-10 12:36:39 +0300

    addComponent

--- gem.client/src/app/app.module.ts
+++ gem.client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Project8Component } from './project8/project8.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Project8Component
   ],
   imports: [
     BrowserModule, HttpClientModule,
---
+++ gem.client/src/app/project8/project8.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project8',
+  templateUrl: './project8.component.html',
+  styleUrl: './project8.component.css'
+})
+export class Project8Component {
+
+}
