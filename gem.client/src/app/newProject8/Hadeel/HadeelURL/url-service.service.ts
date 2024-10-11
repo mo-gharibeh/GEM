e8914f4e2db320cfc090762676587dad48894864
Hadeel-Alzoubi <hadeelzoubi12@gmail.com>
2024-10-11 18:11:10 +0300

    Update Details And Subscribtion

--- gem.client/src/app/newProject8/Hadeel/HadeelURL/url-service.service.ts
+++ gem.client/src/app/newProject8/Hadeel/HadeelURL/url-service.service.ts
@@ -7,17 +7,23 @@ import { Observable } from 'rxjs';
 })
 export class UrlServiceService {
   
-  staticData = "http://localhost:44340/api";
+  staticData = "https://localhost:44340/api";
 
   constructor(private http: HttpClient) { }
 
   getGym(): Observable<any> {
-    
-    return this.http.get<any>(`https://localhost:44340/api/Gym/ShowAllGyms`);
+
+    return this.http.get<any>(`${this.staticData}/Gym/ShowAllGyms`);
 
   }
 
   getGymDetails(id: any): Observable<any> {
+    debugger
     return this.http.get<any>(`${this.staticData}/Gym/ShowGymDetails/${id}`)
+  }
+
+
+  addUSerSubScription(data: any): Observable<any> {
+    return this.http.post<any>(`${this.staticData}/Subscribtion/AddGymSubscribtion`, data)
   }
 }
