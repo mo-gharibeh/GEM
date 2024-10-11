781b6fb6d58b598a61ac806209fc67f74dca6494
lujain998 <lujainalazzam98@gmail.com>
2024-10-11 18:53:29 +0300

    cart

--- gem.client/src/app/newProject8/Lujain/LujainURL/lujain-url.service.ts
+++ gem.client/src/app/newProject8/Lujain/LujainURL/lujain-url.service.ts
@@ -41,56 +41,77 @@ export class LujainURLService {
   }
 
 
+  getCartItem(id: number): Observable<any> {
+    return this.http.get<any>(`${this.staticData}/Cart/Cart/${id}`);
+  }
+
+
+  addCartItem(data: any): Observable<any> {
+    return this.http.post<any>(`${this.staticData}/Cart/Cart`, data)
+  }
+
+  editCartItem(userId: any, cartItemId: any, quantity: number): Observable<any> {
+    return this.http.put<any>(`${this.staticData}/Cart/Cart/${userId}/${cartItemId}`, { quantity });
+  }
+
+  deleteCartItem(userId: any, cartItemId: any): Observable<any> {
+    return this.http.delete<any>(`${this.staticData}/Cart/Cart/${userId}/${cartItemId}`);
+  }
 
   cartItem: any = [];
   cartITemSubject: BehaviorSubject<any> = new BehaviorSubject<any>(this.cartItem);
   cartItemObser = this.cartITemSubject.asObservable();
   addTocart(data: any) {
-
-    debugger;
-
-    var record = this.cartItem.find((x: any) => x.productId == data.productId)
+    var record = this.cartItem.find((x: any) => x.productId == data.productId);
     if (record) {
-      alert("the product already exist");
-    }
-    else {
+      alert("The product already exists");
+    } else {
       this.cartItem.push(data);
       this.cartITemSubject.next(this.cartItem);
+      localStorage.setItem('cartItems', JSON.stringify(this.cartItem));
     }
-
   }
+
+
+
+  pushCartToDatabase(userId: any): Observable<any> {
+    return this.http.post<any>(`${this.staticData}/Cart/Cart`, {
+      userId,
+      cartItems: this.cartItem
+    });
+  }
+
 
 
 
   increaseQuantity(id: any) {
-
-    var product = this.cartItem.find((x: any) => x.productId == id)
-
-
+    const product = this.cartItem.find((x: any) => x.productId == id);
     if (product) {
       product.quantity += 1;
       this.cartITemSubject.next(this.cartItem);
+      localStorage.setItem('cartItems', JSON.stringify(this.cartItem));
     }
   }
 
-
-
   decreaseQuantity(id: any) {
-
-    var product = this.cartItem.find((x: any) => x.productId == id)
-
-
+    const product = this.cartItem.find((x: any) => x.productId == id);
     if (product) {
-      product.quantity -= 1;
-      this.cartITemSubject.next(this.cartItem);
+      if (product.quantity > 1) {
+        product.quantity -= 1;
+        this.cartITemSubject.next(this.cartItem);
+        localStorage.setItem('cartItems', JSON.stringify(this.cartItem));
+      } else {
+        alert("The quantity cannot be less than 1.");
+      }
     }
   }
 
   removeItem(id: any) {
-    // Remove item from the local cart
     this.cartItem = this.cartItem.filter((item: any) => item.productId !== id);
-    this.cartITemSubject.next(this.cartItem); // Notify observers
+    this.cartITemSubject.next(this.cartItem);
+    localStorage.setItem('cartItems', JSON.stringify(this.cartItem));
   }
 }
+
 
 
