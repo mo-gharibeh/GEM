3b5850b6e1336deaa7b64bcd052b54f3e7b9c133
ahmed-yacoub <ahmedyacoub718@gmail.com>
2024-10-13 20:39:16 +0300

    add hadeel payment

---
+++ GEM.Server/Models/PaymentHadeel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GEM.Server.Models;
+
+public partial class PaymentHadeel
+{
+    public int PaymentId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int ClassSubId { get; set; }
+
+    public int ClassTimeId { get; set; }
+
+    public string PaymentToken { get; set; } = null!;
+
+    public decimal Amount { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? CompanyName { get; set; }
+
+    public string? Address { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
