67705a0e313c72add00c06bcdb813666f2d36e86
dimamohamad <deemaahmad189@gmail.com>
2024-10-14 10:33:32 +0300

    last synic

--- GEM.Server/Program.cs
+++ GEM.Server/Program.cs
@@ -1,5 +1,5 @@
 using GEM.Server.Models;
-using GEM.Server.Service;
+//using GEM.Server.Service;
 using GEM.Server.yousefDTO;
 using Microsoft.EntityFrameworkCore;
 using GEM.Server.DTOs;
@@ -13,8 +13,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<MyDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("YourConnectionString")));
+//builder.Services.AddDbContext<MyDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("YourConnectionString")));
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("Development", options =>
@@ -26,26 +26,26 @@ builder.Services.AddCors(options =>
 }); 
 
 builder.Services.AddScoped<IEmailService, GEM.Server.yousefDTO.EmailService>();
-builder.Services.AddScoped<PayPalPaymentService>();
+//builder.Services.AddScoped<PayPalPaymentService>();
 builder.Services.AddScoped<PaymentServiceH>();
 
 
 
-//Configure PayPal settings from appsettings.json
-builder.Services.AddSingleton(sp =>
-{
-    var config = new Dictionary<string, string>
-    {
-        { "clientId", builder.Configuration["PayPal:ClientId"] },
-        { "clientSecret", builder.Configuration["PayPal:ClientSecret"] },
-        { "mode", builder.Configuration["PayPal:Mode"] } // sandbox or live
-    };
+////Configure PayPal settings from appsettings.json
+//builder.Services.AddSingleton(sp =>
+//{
+//    var config = new Dictionary<string, string>
+//    {
+//        { "clientId", builder.Configuration["PayPal:ClientId"] },
+//        { "clientSecret", builder.Configuration["PayPal:ClientSecret"] },
+//        { "mode", builder.Configuration["PayPal:Mode"] } // sandbox or live
+//    };
 
-return new PayPalConfigManager(config);
-});
+//return new PayPalConfigManager(config);
+//});
 
 //Register PayPalPaymentService
-builder.Services.AddTransient<PayPalPaymentService>();
+//builder.Services.AddTransient<PayPalPaymentService>();
 
 builder.Services.AddTransient<GEM.Server.yousefDTO.EmailService>();
 
@@ -53,9 +53,9 @@ builder.Services.AddTransient<GEM.Server.yousefDTO.EmailService>();
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
 
-builder.Configuration
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+//builder.Configuration
+//    .SetBasePath(Directory.GetCurrentDirectory())
+//    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 
 var app = builder.Build();
